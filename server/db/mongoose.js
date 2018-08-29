ed263036c0e95fc2d708b3e371deca91f40f2655
Aloke Deep Ganguly <alokedeepganguly@gmail.com>
2018-08-29 22:32:11 +0530

    Making Database Generic

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://Aloke:qwerty123@ds235732.mlab.com:35732/node_todo_app_db');
+ if (process.env.port){
+   var connStr='mongodb://Aloke:qwerty123@ds235732.mlab.com:35732/node_todo_app_db'
+ }else{
+   var connStr='mongodb://127.0.0.1:27017/TodoApp'
+ }
+
+
+mongoose.connect(connStr);
 
 module.export={mongoose};
