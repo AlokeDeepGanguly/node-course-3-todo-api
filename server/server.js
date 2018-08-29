bc0be5a223e398c891763fc01ddef8c0449cc5b2
Aloke Deep Ganguly <alokedeepganguly@gmail.com>
2018-08-29 23:48:54 +0530

    Configured Delete/ :id route

--- server/server.js
+++ server/server.js
@@ -45,6 +45,23 @@ return  res.status(404).send();
   })
 })
 
+app.delete('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+return  res.status(404).send();
+   }
+   
+   Todo.findByIdAndRemove(id).then((todo)=>{
+     if(!todo){
+   return  res.status(404).send('Todo not Found');
+      }
+     return res.status(200).send(todo);
+   },(err)=>{
+     res.status(400).send();
+   })
+})
+
 app.listen(port, () =>{
   console.log(`Started on port: ${port}`);
 })
