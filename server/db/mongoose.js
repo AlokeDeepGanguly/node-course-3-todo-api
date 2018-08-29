d73884a8325a3600f3e39e3e73691f8c7339911c
Aloke Deep Ganguly <alokedeepganguly@gmail.com>
2018-08-29 22:36:08 +0530

    Reverting to previous version

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise=global.Promise;
- if (process.env.port){
-   var connStr='mongodb://Aloke:qwerty123@ds235732.mlab.com:35732/node_todo_app_db'
- }else{
-   var connStr='mongodb://127.0.0.1:27017/TodoApp'
- }
-
-
-mongoose.connect(connStr);
+mongoose.connect('mongodb://Aloke:qwerty123@ds235732.mlab.com:35732/node_todo_app_db');
 
 module.export={mongoose};
